bc9784d3c98c996df31cbe97cd99da49e9eeefc1
milanpajovic <milan.pajovic98@gmail.com>
2023-11-16 14:50:23 +0300

    configure database

---
+++ server/src/config/index.ts
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export enum ApplicationEnv {
+  PRODUCTION = 'production',
+  DEVELOPMENT = 'development',
+  STAGING = 'staging',
+  TEST = 'test',
+}
+
+const {
+  NODE_PORT,
+  NODE_HOST,
+  DB_DIALECT,
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  NODE_ENV,
+  SESSION_SECRET,
+} = process.env;
+
+const ENV: ApplicationEnv =
+  (NODE_ENV as ApplicationEnv) || ApplicationEnv.DEVELOPMENT;
+
+export const CONFIG = {
+  NODE_PORT,
+  NODE_HOST,
+  NODE_ENV: ENV,
+  DB_DIALECT,
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  SESSION_SECRET,
+};
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,10 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 import { initTracing } from '../tracing/tracer';
 const openTelemetrySDK = initTracing('api-service');
 openTelemetrySDK.start();
 
 import express from 'express';
 import * as path from 'path';
+import db from './models';
 
 const app = express();
 
@@ -17,8 +14,25 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to server!' });
 });
 
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+const startServer = async () => {
+  try {
+    await db
+      .authenticate({})
+      .then(() => console.log('Connection has been established successfully.'))
+      .catch((error) =>
+        console.log('Unable to connect to the database:', error)
+      );
+    // Start the Express server
+    const port = process.env.PORT || 3000;
+    const server = app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}/api`);
+    });
+    server.on('error', console.error);
+  } catch (error) {
+    console.error(error);
+    // todo
+    process.exit(1);
+  }
+};
+
+startServer();

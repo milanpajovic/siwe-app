cdd7e224b44d2818c55c60c6c720422b02c25502
Milan Pajovic <milan.pajovic98@gmail.com>
2023-11-20 00:24:29 +0300

    update main workflow (#22)

--- server/src/middleware/session.ts
+++ server/src/middleware/session.ts
@@ -1,13 +1,13 @@
 import { IronSessionOptions } from 'iron-session';
 import { SiweMessage } from 'siwe';
 import { ironSession } from 'iron-session/express';
-import { CONFIG, ApplicationEnv } from '../config';
+import { CONFIG } from '../config';
 
 const sessionOptions: IronSessionOptions = {
   cookieName: CONFIG.COOKIE_NAME,
   password: CONFIG.SECRET_COOKIE_PASSWORD,
   cookieOptions: {
-    secure: CONFIG.NODE_ENV === ApplicationEnv.PRODUCTION,
+    secure: false,
   },
 };
 
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,20 +1,25 @@
 import { Sequelize } from 'sequelize-typescript';
-import { CONFIG } from '../config';
+import { ApplicationEnv, CONFIG } from '../config';
 import Profile from './Profile.model';
+
+const dialectOptions =
+  CONFIG.NODE_ENV === ApplicationEnv.PRODUCTION
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
 const db = new Sequelize({
   database: CONFIG.DB_NAME,
   host: CONFIG.DB_HOST,
   port: +CONFIG.DB_PORT,
-  dialect: CONFIG.DB_DIALECT as never, // todo
+  dialect: CONFIG.DB_DIALECT as never,
   username: CONFIG.DB_USER,
   password: CONFIG.DB_PASSWORD,
   models: [Profile],
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  dialectOptions,
   logging: (query) => console.log(query),
 });
 export default db;
